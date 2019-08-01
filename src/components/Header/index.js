01625992d597567c1c5e2b6e3463a73496499037
Samuel Brehm <samuelbrehm@gmail.com>
2019-08-01 11:01:20 -0300

    Fix avatar read header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -29,8 +29,9 @@ export default function Header() {
             </div>
             <img
               src={
-                profile.avatar.url ||
-                'https://api.adorable.io/avatars/51/abott@adorable.png'
+                profile.avatar
+                  ? profile.avatar.url
+                  : `https://api.adorable.io/avatars/50/${profile.name}.png`
               }
               alt="Samuel Brehm"
             />
